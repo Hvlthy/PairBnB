b0fc0fc7084e00cfec44dca22188fc2886be9f99
JonaireFrancis <jonfrancis.junior@gmail.com>
2024-09-19 12:06:00 -0400

    cookie test

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,21 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
-
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
   });
+// GET /api/set-token-cookie
+  router.get('/set-token-cookie', async (_req, res) => {
+    const user = await User.findOne({
+      where: {
+        username: 'Demo-lition'
+      }
+    });
+    setTokenCookie(res, user);
+    return res.json({ user: user });
+  });
+  
   
 module.exports = router;
